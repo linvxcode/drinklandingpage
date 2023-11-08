2530124b11950ed17155e73315cfde0b8619c5eb
linxcode <linxmusic.com>
2023-11-08 14:41:51 +0700

    init

---
+++ src/common/module/homepage/component/Mainpage.tsx
@@ -0,0 +1,22 @@
+import Image from "@/common/component/element/Image";
+import React from "react";
+
+const Mainpage = () => {
+  return (
+    <div className="flex justify-between py-[150px]  px-20 max-w-[1500px]">
+      <div>
+        <h1 className="lg:text-7xl font-bold w-[50%]">ENERGY DRINK</h1>
+        <h2 className="w-[50%] mt-3 leading-relaxed">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry&rsquo;s standard dummy text ever
+          since the 1500s,
+        </h2>
+      </div>
+      <div className="w-[20%]">
+        <h1>ashkjdasd</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Mainpage;
---
+++ src/common/module/homepage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Mainpage from './component/Mainpage'
+
+const Homepage = () => {
+  return (
+    <div className='h-screen bg-color'>
+      <Mainpage/>
+    </div>
+  )
+}
+
+export default Homepage
